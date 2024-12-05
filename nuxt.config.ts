252d4017f49e214288d24149b5a6a150b57ef8ae
SteveMadoerin <steve.madoerin@htwg-konstanz.de>
2024-12-06 00:02:22 +0100

    pwa added to nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,36 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  //ssr: true,
+  app: {
+    head: {
+      title: 'minesweeper-nuxt', // App window nav title
+      
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'theme-color', content: '#121212' },
+        // ...
+      ],
+      link: [
+        { rel: 'manifest', href: 'manifest.json' },
+        { rel: 'apple-touch-icon', href: 'apple-touch-icon.png' },
+      ],
+    },
+    pageTransition: { name: 'page', mode: 'out-in' }
+  },
   modules: ['@vite-pwa/nuxt', '@pinia/nuxt'],
+
   pwa: {  
+    registerType: 'autoUpdate',
+    includeManifestIcons: true,
+    includeAssets: [
+      'favicon.ico',
+      'apple-touch-icon-180x180.png',
+      'maskable-icon-512x512.png',
+      'minesweeper.mp4',
+    ],
     manifest: {
       name: 'minesweeper-nuxt',
-      short_name: 'Minesweeper',
+      lang: 'en',
       start_url: '/',
       background_color: '#ffffff',
       orientation: 'landscape',
@@ -36,9 +59,40 @@ export default defineNuxtConfig({
         },
       ],
     },
-  },	
+    workbox: {
+      runtimeCaching: [
+        {
+          urlPattern: ({ request }) =>
+            request.destination === 'style' ||
+            request.destination === 'script' ||
+            request.destination === 'font' ||
+            request.destination === 'worker',
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'static-resources',
+            expiration: {
+              maxEntries: 500,
+              maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+            },
+          },
+        },
+        {
+          urlPattern: ({ request }) => request.destination === 'image',
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images',
+            expiration: {
+              maxEntries: 500,
+              maxAgeSeconds: 42 * 24 * 60 * 60, // 42 days
+            },
+          },
+        },
+      ],
+      skipWaiting: true,
+      cleanupOutdatedCaches: true,
+      //clientsClaim: true,
+    },
+  },
 
-  
-  
-
+  compatibilityDate: '2024-12-05',
 })
