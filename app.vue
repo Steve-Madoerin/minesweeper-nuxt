224db0e38f3c43bf9a83e46fd87a59872fdfef62
SteveMadoerin <steve.madoerin@htwg-konstanz.de>
2024-12-05 15:41:53 +0100

    invisible

--- app.vue
+++ app.vue
@@ -1,15 +1,21 @@
 <template>
-  <div id ="app">
+  <div id="app">
     <header>
-      <nav>
+      <nav class="invisible">
         <NuxtLink to="/">Home</NuxtLink>
         <NuxtLink to="/rules">Rules</NuxtLink>
         <NuxtLink to="/game">Game</NuxtLink>
         <NuxtLink to="/chat">Chat</NuxtLink>
-        <NuxtLink to ="/offline">Offline</NuxtLink>
+        <NuxtLink to="/offline">Offline</NuxtLink>
       </nav>
     </header>
 
     <NuxtPage />
   </div>
 </template>
+
+<style scoped>
+.invisible {
+  display: none;
+}
+</style>
