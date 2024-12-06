91fdb577cde296d1f90876cb15810f85dc382f3a
SteveMadoerin <steve.madoerin@htwg-konstanz.de>
2024-12-06 15:32:00 +0100

    simple auth via google

--- pages/login.vue
+++ pages/login.vue
@@ -1,3 +1,73 @@
-<template></template>
-<script setup lang="ts"></script>
-<style scoped></style>
+<!-- pages/login.vue -->
+
+<template>
+    <div class="login-container">
+      <h1>Login</h1>
+      <!-- <form @submit.prevent="login"> -->
+        <!-- <input type="email" v-model="email" placeholder="Email" required />
+        <input type="password" v-model="password" placeholder="Password" required /> -->
+        <!-- <button type="submit">Log in to continue</button> -->
+      <!-- </form> -->
+      <button class="google-button">Log in to continue</button>
+      <button @click="signInWithGoogle" class="google-button">Sign in with Google</button>
+      <!-- <p v-if="errorMessage" class="error">{{ errorMessage }}</p> -->
+    </div>
+  </template>
+  
+  <script setup lang="ts" >
+
+  import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+  const auth = useFirebaseAuth()
+
+  function signInWithGoogle() {
+    signInWithPopup(auth!, new GoogleAuthProvider())
+}
+  </script>
+  
+  <style scoped>
+  .login-container {
+    max-width: 400px;
+    margin: 0 auto;
+    padding: 2rem;
+    background-color: #ffffff;
+    border-radius: 8px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    text-align: center;
+  }
+  
+  input {
+    width: 100%;
+    padding: 0.5rem;
+    margin: 0.5rem 0;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+  
+  button {
+    width: 100%;
+    padding: 0.5rem;
+    margin: 0.5rem 0;
+    background-color: #007bff;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+  
+  button:hover {
+    background-color: #0056b3;
+  }
+  
+  .google-button {
+    background-color: #db4437;
+  }
+  
+  .google-button:hover {
+    background-color: #c23321;
+  }
+  
+  .error {
+    color: red;
+    margin-top: 1rem;
+  }
+  </style>
---
+++ plugins/firebase.ts
@@ -0,0 +1,21 @@
+/* // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDfE5mnEwDbsPLhJEMk-ymvndD8yNpJ6L4",
+  authDomain: "minesweeper-firebase-cbd5b.firebaseapp.com",
+  projectId: "minesweeper-firebase-cbd5b",
+  storageBucket: "minesweeper-firebase-cbd5b.firebasestorage.app",
+  messagingSenderId: "709061175495",
+  appId: "1:709061175495:web:694d6bff4d9274f02bb6e2"
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+
+export default auth */
